54098c1aee5a9394965316a5d960dd25b5e350f4
Anang <tyoandini@gmail.com>
2021-07-21 06:16:01 +0700

    layouting system

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ANANG Blog | Posts</title>
-</head>
+@section('container')
+<h1>Halaman Posts</h1>
 
-<body>
-    <h1>Halaman Posts</h1>
-</body>
 
-</html>
+@endSection
