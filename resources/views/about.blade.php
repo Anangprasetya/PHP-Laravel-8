24d6f610272b554b0d893845b48ba9db2c9421a2
Anang <tyoandini@gmail.com>
2021-07-20 08:26:17 +0700

    syntak blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,9 +11,9 @@
 
 <body>
     <h1>Halaman About</h1>
-    <h3><?= $name; ?></h3>
-    <p><?php echo $email; ?></p>
-    <img src="img/<?php echo $image; ?>" alt="Anang" width="200">
+    <h3>{{ $name }} </h3>
+    <p>{{ $email }} </p>
+    <img src="img/{{$image}} " alt="Anang" width="200">
 </body>
 
 </html>
