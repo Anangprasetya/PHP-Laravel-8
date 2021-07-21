ae7219a45d092bfd1094e01351a96694a9832c97
Anang <tyoandini@gmail.com>
2021-07-22 06:35:58 +0700

    array collection

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,7 +13,7 @@ class Post
             "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit inventore et eaque commodi, asperiores at saepe! Perferendis, ducimus possimus obcaecati suscipit nisi commodi quo corporis recusandae aperiam totam voluptate unde ea eum sapiente necessitatibus veritatis sed sunt architecto dolorem delectus. Aliquid temporibus, provident iure maiores sunt omnis reprehenderit aliquam sapiente corporis, magnam expedita officiis pariatur dicta itaque nemo excepturi dolorem maxime quod. Veritatis vel voluptatibus molestiae deserunt earum ratione nostrum fugit, iure assumenda consectetur officia! Dolor vitae architecto sit mollitia."
         ],
         [
-            "title" => "Judul Post Anang",
+            "title" => "Judul Post Kedua",
             "slug" => "judul-post-kedua",
             "author" => "DoDwi",
             "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, harum! Fuga culpa similique omnis non? Ipsa nisi non odit mollitia optio blanditiis iusto labore modi atque! Saepe delectus odit suscipit illo officiis officia iusto quae. Architecto reiciendis delectus impedit quia ea iusto voluptas iste consectetur tempora, ullam laudantium tempore autem assumenda fuga minus! Nisi repellat assumenda rem exercitationem quis est nostrum autem similique deleniti dolores fugit amet expedita, totam ratione maiores quod hic dolorem repudiandae. Deleniti ab eveniet nesciunt accusamus sed fuga ratione incidunt quisquam. Quam atque ratione obcaecati aut magni nobis ad, similique distinctio eaque, libero ex corrupti laudantium architecto dolores aliquid saepe est assumenda recusandae repellendus deleniti voluptatem asperiores blanditiis eum? Ipsa sequi impedit minima ea, a repellat?"
@@ -22,22 +22,12 @@ class Post
 
     public static function all()
     {
-        return self::$blog_post;
+        return collect(self::$blog_post);
     }
 
     public static function find($slug)
     {
-        $posts = self::$blog_post;
-
-        $new_post = [];
-
-        foreach ($posts as $x) {
-            if ($x["slug"] == $slug) {
-                $new_post = $x;
-            }
-        }
-
-
-        return $new_post;
+        $posts = static::all();
+        return $posts->firstWhere('slug', $slug);
     }
 }
