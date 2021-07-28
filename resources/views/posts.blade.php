5b31896241f1ecf01b490771b7c86d802071a20d
Anang <tyoandini@gmail.com>
2021-07-28 18:46:57 +0700

    memperbaiki view post dan posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,10 +4,10 @@
 @foreach($posts as $x)
 <article class="mb-5">
     <h2>
-        <a href="/posts/{{ $x['slug'] }}">{{ $x["title"] }}</a>
+        <a href="/posts/{{ $x->id }}">{{ $x->title }}</a>
     </h2>
-    <h5>By: {{ $x["author"] }} </h5>
-    <p>{{ $x["body"] }} </p>
+    <!-- <h5>By: {{ $x["author"] }} </h5> -->
+    <p>{{ $x->excerpt }} </p>
 </article>
 @endforeach
 @endSection
