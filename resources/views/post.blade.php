ba339417569ed0307550941ba42233aeec2ef3c2
Anang <tyoandini@gmail.com>
2021-07-31 06:45:40 +0700

    relationship post dan user serta inverse

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
 <article>
     <h1>{{ $post->title }} </h1>
-    <p>By. Anang Nur Prasetya in <a href="/categories/{{$post->category->slug}}"> {{ $post->category->name }} </a> </p>
+    <p>By. <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none"> {{ $post->category->name }} </a> </p>
     <!-- <h5>{{ $post->author }} </h5> -->
     <!-- {{ $post->body }} -->
     {!! $post->body !!}
 </article>
 
-<a href="/posts">Back to Posts</a>
+<a href="/posts" class="d-block mt-5">Back to Posts</a>
 @endSection
