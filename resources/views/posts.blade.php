f4e585f4d830e3e99cb889d71f8600cb5234b8d2
Anang <tyoandini@gmail.com>
2021-07-31 06:40:31 +0700

    membuat seeder

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 @foreach($posts as $x)
 <article class="mb-5 border-bottom pb-3">
     <h2><a href="/posts/{{ $x->slug }}" class="text-decoration-none">{{ $x->title }}</a></h2>
-    <p>By. Anang Nur Prasetya in <a href="/categories/{{$x->category->slug}}" class="text-decoration-none"> {{ $x->category->name }} </a> </p>
+    <p>By. <a href="">{{ $x->user->name }}</a> in <a href="/categories/{{$x->category->slug}}" class="text-decoration-none"> {{ $x->category->name }} </a> </p>
     <!-- <h5>By: {{ $x["author"] }} </h5> -->
     <p>{{ $x->excerpt }} </p>
 
