314cbd116bd5500a4e66bc4bb982f5a3c4182bdd
Anang <tyoandini@gmail.com>
2021-07-31 05:48:26 +0700

    memperbaiki view  posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.main')
 
 @section('container')
+
+<h1 class="mb-5">Halaman Blog Posts</h1>
 @foreach($posts as $x)
-<article class="mb-5">
-    <h2>
-        <a href="/posts/{{ $x->slug }}">{{ $x->title }}</a>
-    </h2>
+<article class="mb-5 border-bottom pb-3">
+    <h2><a href="/posts/{{ $x->slug }}" class="text-decoration-none">{{ $x->title }}</a></h2>
+    <p>By. Anang Nur Prasetya in <a href="/categories/{{$x->category->slug}}" class="text-decoration-none"> {{ $x->category->name }} </a> </p>
     <!-- <h5>By: {{ $x["author"] }} </h5> -->
     <p>{{ $x->excerpt }} </p>
+
+    <a href="/posts/{{ $x->slug }}" class="text-decoration-none">Read more...</a>
 </article>
 @endforeach
 @endSection
