9f536268d66b78dab82d28173c497860084d624c
Anang <tyoandini@gmail.com>
2021-08-01 07:03:10 +0700

    alias user_id dengan author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 @foreach($posts as $x)
 <article class="mb-5 border-bottom pb-3">
     <h2><a href="/posts/{{ $x->slug }}" class="text-decoration-none">{{ $x->title }}</a></h2>
-    <p>By. <a href="" class="text-decoration-none">{{ $x->user->name }}</a> in <a href="/categories/{{$x->category->slug}}" class="text-decoration-none"> {{ $x->category->name }} </a> </p>
+    <p>By. <a href="/authors/{{$x->author->username}}" class="text-decoration-none">{{ $x->author->name }}</a> in <a href="/categories/{{$x->category->slug}}" class="text-decoration-none"> {{ $x->category->name }} </a> </p>
     <!-- <h5>By: {{ $x["author"] }} </h5> -->
     <p>{{ $x->excerpt }} </p>
 
