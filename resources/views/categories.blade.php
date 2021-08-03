baa7854f88247e3b7a494767950bca1e14fb231e
Anang <tyoandini@gmail.com>
2021-08-03 09:06:50 +0700

    memberi tampilan all category

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,11 +2,23 @@
 
 @section('container')
 <h1 class="mb-5">Post Category </h1>
-@foreach ($categories as $x)
-<ul>
-    <li>
-        <h2><a href="/categories/{{ $x->slug }}">{{ $x->name }}</a></h2>
-    </li>
-</ul>
-@endforeach
+<div class="container">
+    <div class="row">
+        @foreach ($categories as $x)
+        <div class="col-md-4">
+            <a href="/categories/{{$x->slug}}">
+                <div class="card bg-dark">
+                    <img src="https://source.unsplash.com/500x500?{{$x->name}}" class="card-img" alt="{{$x->name}}"">
+                    <div class=" card-img-overlay d-flex align-items-center p-0">
+                    <h5 class="card-title text-center flex-fill p-4 fs-3 text-white" style="background-color: rgba(0, 0, 0, 0.9);">{{ $x->name }} </h5>
+                </div>
+            </a>
+        </div>
+    </div>
+    @endforeach
+</div>
+</div>
+
+
+
 @endSection
