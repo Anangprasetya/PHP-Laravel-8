ded825f545a204f077a893b3d47be8a32443b350
Anang <tyoandini@gmail.com>
2021-08-14 06:44:18 +0700

    old data

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -9,7 +9,7 @@
             <form action="/register" method="POST">
                 @csrf
                 <div class="form-floating">
-                    <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" id="name" placeholder="name">
+                    <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" id="name" placeholder="name" required value="{{ old('name') }}">
                     <label for="name">Name</label>
                     @error('name')
                     <div class="invalid-feedback">
@@ -18,8 +18,8 @@
                     @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="username">
-                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="username" required value="{{ old('username') }}">
+                    <label for=" username">Username</label>
                     @error('username')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -27,8 +27,8 @@
                     @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com">
-                    <label for="email">Email address</label>
+                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
+                    <label for=" email">Email address</label>
                     @error('email')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -36,8 +36,8 @@
                     @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" placeholder="Password">
-                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
+                    <label for=" password">Password</label>
                     @error('password')
                     <div class="invalid-feedback">
                         {{ $message }}
