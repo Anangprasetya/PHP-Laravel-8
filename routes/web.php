894b27b1d7b60f9c1e76ea5aa5a459323999bf17
Anang <tyoandini@gmail.com>
2021-08-18 05:42:10 +0700

    validasi form login

--- routes/web.php
+++ routes/web.php
@@ -66,5 +66,6 @@ Route::get('/authors/{author:username}', function (User $author) {
 
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
