4d4aed00d1f1f51d72b54ad6ee5aa67c8e947196
Anang <tyoandini@gmail.com>
2021-08-18 08:30:47 +0700

    route name

--- routes/web.php
+++ routes/web.php
@@ -66,11 +66,11 @@ Route::get('/authors/{author:username}', function (User $author) {
 });
 
 
-Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
-Route::post('/logout', [LoginController::class, 'logout']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
