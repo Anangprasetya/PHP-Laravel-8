7171c988a0fd68615f42f2e94ab29403f90ce9f7
Anang <tyoandini@gmail.com>
2021-08-25 07:35:27 +0700

    create page detail post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -5,7 +5,11 @@
         <div class="col-lg-8">
             <h1>{{ $post->title }} </h1>
 
-            <img src="https://source.unsplash.com/1200x400?{{$post->category->name}}" alt="{{$post->category->name}}" class="img-fluid">
+            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my posts</a>
+            <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+
+            <img src="https://source.unsplash.com/1200x400?{{$post->category->name}}" alt="{{$post->category->name}}" class="img-fluid mt-2">
 
             <article class="my-3 fs-5">
                 {!! $post->body !!}
