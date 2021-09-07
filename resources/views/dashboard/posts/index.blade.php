b3cd083268e940e03128d3dadf6eace35e57a980
Anang <tyoandini@gmail.com>
2021-09-08 06:40:03 +0700

    delete data

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,7 +5,7 @@
 </div>
 
 @if(session()->has('success'))
-<div class="alert alert-success" role="alert">
+<div class="alert alert-success col-lg-9" role="alert">
     {{ session('success') }}
 </div>
 @endif
@@ -30,7 +30,11 @@
                 <td>
                     <a href="/dashboard/posts/{{ $x->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
                     <a href="#" class="badge bg-warning"><span data-feather="edit"></span></a>
-                    <a href="#" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+                    <form action="/dashboard/posts/{{ $x->slug }}" method="POST" class="d-inline">
+                        @method('delete')
+                        @csrf
+                        <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span></button>
+                    </form>
                 </td>
             </tr>
             @endforeach
